78d879ef9831ac42d93ccb2d744621f7f0dcb38e
kira076 <j.morrisoncleary@gmail.com>
2020-07-06 00:33:09 +1000

    added sidebar
    Sidebar for primary navigation use

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header/>
     <Home/>
+    <Sidebar/>
   </div>
 </template>
 
 <script>
 import Home from './views/Home'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 export default {
   name: 'App',
   components: {
     Home,
-    Header
+    Header,
+    Sidebar
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">Header Bar</b-navbar-brand>
+      <b-button v-b-toggle.sidebar-1>Sidebar</b-button>
     </b-navbar>
   </div>
 </template>
