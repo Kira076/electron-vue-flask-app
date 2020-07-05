3f45b1068fee2d5271e3fcbc3d729f5a0da6c678
kira076 <j.morrisoncleary@gmail.com>
2020-07-05 15:32:03 +1000

    reinstalling vue-bootstrap

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Header Bar</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
